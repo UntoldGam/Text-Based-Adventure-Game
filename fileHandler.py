3458662389600fba4f0527a5517bc2daef1da9d7
UntoldGam <charlessbird+github@outlook.com>
2023-01-09 19:59:02 +0000

    optimised fileHandler (DRY Principle)

--- fileHandler.py
+++ fileHandler.py
@@ -8,63 +8,37 @@ def validate(path):
 		return False
 
 def openFile(filePath, method, fileContent):
-	if validate(path):
 		if method:
 			with open(filePath, method) as file:
-				action = "write" if method == "w" else "read"
 				if method == "w":
 					file.write(dumps(fileContent))
 				elif method == "r":
 					return loads(file.read())
-			
+	
 def newSave(username, data):
 	path = f"./saves/{username}.json"
-	if validate(path):
-		openFile(path)
-	if not validate(f"./saves/{name}.json"):
-		openFile(f"./saves/{name}.json", "w", data)
+	openFile(path) if validate(path) else openFile(path, "w", data)
 
 def loadSave(username):
 	path = f"./saves/{username}.json"
-	if validate(path):
-		openFile(path)
+	message = ""
+	openFile(path) if validate(path) else print("No save found")
 
-
-	if isfile(f"./saves/{username}.json"):
-		file = open(f"./saves/{username}.json")
-		content = loads(file.read())
-		file.close()
-		return content	
-	else:
-		return False
-
-
-	
 def fetchUser(username):
-	if isfile(f"./users/{username}.json"):
-		with open(f"./users/{username}.json", "r") as file:
-			content = loads(file.read())
-			return content
-	else:
-		return False
+	path = f"./users/{username}.json"
+	openFile(path) if validate(path) else openFile(path, "w", data)
 
 def newUser(data):
-		file = open(f"./users/{data.get('userName')}.json","w")
-		file.write(dumps(data))
-		file.close() 
-		return True
+	path = f"./users{data['username']}.json"
+	method = "w"
+	openFile(path, method, data)
 
 def saveUser(data):
-	if isfile(f"./users/{data.get('userName')}.json"):
-		file = open(f"./users/{data.get('userName')}.json","r")
-		content = loads(file.read())
+	path = f"./users{data['username']}.json"
+	if validate(path):
 		newData = {}
 		for key in data:
-			#print(content, data.get(key))
 			newData.update({ f"{key}": data.get(key) })
-			#print(newData)
-		file = open(f"./users/{data.get('userName')}.json","w")
-		file.write(dumps(newData))
-		file.close()
-	else:
+		openFile(path, "w", newData)
+	elif not validate(path):
 		newUser(data)
